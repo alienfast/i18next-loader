5bb2ae0c017ebc22337535e7a6160953082d8174
Kevin Ross <kevin.ross@alienfast.com>
2022-11-07 12:35:52 -0600

    Update ci, yarn, dependencies, and fix vulnerable dependency (#24)
    * Update yarn and move ci to intuit/auto
    
    * Apply yarn.lock changes
    
    * Update all dependencies except loader-utils
    
    * Update loader-utils to 2.0.1 - need breaking change to 3.x
    
    * Drop loader-utils, update tests to mock getOptions
    
    * Try to force canary publishing to npm
    
    * direct copy publish config from https://github.com/intuit/auto/tree/main/plugins/npm#still-getting-errors
    
    * force registry in .npmrc?
    
    * omit @alienfast scope from .yarrrc.yml and .npmrc
    
    Co-authored-by: rosskevin <rosskevin@users.noreply.github.com>

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -1,56 +1,62 @@
-const path = require('path')
-const chai = require('chai')
-const assert = chai.assert
-const loader = require('../index')
+const path = require("path");
+const chai = require("chai");
+const assert = chai.assert;
+const loader = require("../index");
 
-describe('basic', function () {
-  [ 'yaml', 'json' ].forEach((type) => {
+describe("basic", function () {
+  ["yaml", "json"].forEach((type) => {
     context(type, () => {
       beforeEach(function (done) {
         //mock webpack loader this scope
-        const emptFn = function () {}
+        const emptFn = function () {};
         thisScope = {
           addDependency: emptFn,
           addContextDependency: emptFn,
           cacheable: emptFn,
-          resource: path.join(__dirname, `./data/basic-app-${type}/locales/index.js`)
-        }
-        done()
-      })
+          getOptions: emptFn,
+          resource: path.join(
+            __dirname,
+            `./data/basic-app-${type}/locales/index.js`
+          ),
+        };
+        done();
+      });
 
-      function assertCommon (resStore) {
-        assert.strictEqual(resStore.dev.main.test, 'Dev dev dev!')
-        assert.strictEqual(resStore.de.main.test, 'Das ist ein Test!')
-        assert.strictEqual(resStore.en.main.test, 'This is a test!')
-        assert.strictEqual(resStore.fr.main.test, 'Ceci est un test!')
+      function assertCommon(resStore) {
+        assert.strictEqual(resStore.dev.main.test, "Dev dev dev!");
+        assert.strictEqual(resStore.de.main.test, "Das ist ein Test!");
+        assert.strictEqual(resStore.en.main.test, "This is a test!");
+        assert.strictEqual(resStore.fr.main.test, "Ceci est un test!");
       }
 
-      it('should generate the structure', function () {
-        const res = loader.call(thisScope, 'index.js')
-        const resStore = eval(res)
-        assertCommon(resStore)
-      })
+      it("should generate the structure", function () {
+        const res = loader.call(thisScope, "index.js");
+        const resStore = eval(res);
+        assertCommon(resStore);
+      });
 
-      it('should process include', function () {
-        thisScope.query = `?{include: ['**/*.json']}`
+      it("should process include", function () {
+        thisScope.getOptions = () => ({ include: ["**/*.json"] });
         thisScope.addDependency = function (path) {
-          assert.notInclude(path, 'main.nonjson')
-        }
+          assert.notInclude(path, "main.nonjson");
+        };
 
-        const res = loader.call(thisScope, 'index.js')
-      })
+        const res = loader.call(thisScope, "index.js");
+      });
 
-      it('should not process files that are excluded', function () {
-        thisScope.query = `?{include: ['**/*.${type}', '!**/exclude.${type}']}`
+      it("should not process files that are excluded", function () {
+        thisScope.getOptions = () => ({
+          include: [`**/*.${type}`, `!**/exclude.${type}`],
+        });
         thisScope.addDependency = function (path) {
-          assert.notInclude(path, 'exclude.json')
-        }
+          assert.notInclude(path, "exclude.json");
+        };
 
-        const res = loader.call(thisScope, 'index.js')
-        const resStore = eval(res)
-        assert.strictEqual(resStore.de.main.foo, undefined)
-        assertCommon(resStore)
-      })
-    })
-  })
-})
+        const res = loader.call(thisScope, "index.js");
+        const resStore = eval(res);
+        assert.strictEqual(resStore.de.main.foo, undefined);
+        assertCommon(resStore);
+      });
+    });
+  });
+});
